079f67da541401a17a53448a84be9b5ebfc35c99
Thiago Navarro <thiago@oxyoy.com>
2023-03-15 15:45:41 +0000

    Report finished

--- src/ohMyForm_analyze/report.nim
+++ src/ohMyForm_analyze/report.nim
@@ -1,5 +1,7 @@
 from std/tables import Table, `[]`
 from std/json import items, `{}`, JsonNode, len
+from std/strutils import multiReplace
+from std/uri import decodeUrl
 import karax / [karaxdsl, vdom]
 
 import ohMyForm_analyze/core
@@ -15,6 +17,24 @@ proc createReport*(scores: Table[string, int]; entry: JsonNode): string =
         th: text field{"field", "title"}.getStr
         th: text field{"value"}.getStr
         th: text $scores.getScore field.getId
+  result = """<style>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
 
-  result = $ $vnode
+th, td {
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {background-color: #f2f2f2;}
+</style>
+"""
+  result.add decodeUrl ($($vnode)).multiReplace({
+    "</#text>": "",
+    "<#text>": "",
+    "\n": "",
+    "  ": " "
+  })
   echo result
